4992cafd363214150e1a579bbb6f67ef94aced7d
mattmg83 <mattg83@gmail.com>
2023-03-13 17:55:59 -0500

    Exit function added
    exit + intro and some formatting changes for improved readability

--- MattGPT.py
+++ MattGPT.py
@@ -1,21 +1,25 @@
 import os
 import openai
 
+print('Welcome to MathieuGPT, you\'ll need an OpenAI API key (https://platform.openai.com)\nThis script requires python 3.x and the openai package to run\nWrite your queries in English or French and input \'exit\' at any point to exit and return to the terminal')
 openai.organization = "org-vNip1LEkzDa2rbFClEkXncDz"
-openai.api_key = input("ChatGPT API Key: ")
+openai.api_key = input("\nChatGPT API Key: ")
 
 messages=[]
-messages.append({"role": "system", "content": "you are an assistant with a french accent, you swear in english occasionally, and often respond sarcastically"})
+messages.append({"role": "system", "content": "you are an assistant with a french accent, you occasionally swear in english and often respond sarcastically"})
 
 while True:
-    content = input("User: ")
-    messages.append({"role": "user", "content": content})
-    completion = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=messages
+    content = input("\n>>> User: ")
+    if content != "exit":
+      messages.append({"role": "user", "content": content})
+      completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
 
-    )
+      )
 
-    chat_response = completion.choices[0].message.content
-    print(f'MathieuGPT: {chat_response}')
-    messages.append({"role": "assistant", "content": chat_response})
+      chat_response = completion.choices[0].message.content
+      print(f'\n>>> MathieuGPT: {chat_response}')
+      messages.append({"role": "assistant", "content": chat_response})
+    else:
+      break
